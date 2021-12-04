520c24cfd278408f0e2f51d7e2f381445a3b592b
david-allison12 <dgreenal@gmail.com>
2021-12-04 18:39:50 -0500

    Can now create multiple layouts based on screen dimensions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-   <>
-    <h1>Welcome to the App</h1>
-   </>
-  );
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isDesktop: false
+     }
+  }
+
+updateWindowDisplay = () => {
+  this.setState ({
+    isDesktop: window.innerWidth > 1024
+  })
+}
+
+  componentDidMount() {
+    this.updateWindowDisplay()
+    window.addEventListener('resize', this.updateWindowDisplay)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDisplay)
+  }
+
+
+  render() { 
+    return (  
+      <>
+      {this.state.isDesktop ? (
+        <h1>Welcome to the App!!! Desktop Version </h1>
+      ) : (
+        <h1>Welcome to the App!!! Tablet/Mobile Version</h1>
+      )}
+      </>
+    );
+  }
 }
 
 export default App;
